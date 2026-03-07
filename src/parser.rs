5059bf8572a1e8558b781df4f5a1190910bfbee6
mladicstefan <stefan@mladicstefan.com>
2026-03-07 21:44:14 +0100

    peek and consume

--- src/parser.rs
+++ src/parser.rs
@@ -4,16 +4,33 @@ use crate::{
     lexer::Token,
 };
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Position {
     pub line: usize,
     pub col: usize,
 }
 
+type Lexme = (Token, Position);
+
+#[derive(Clone, Debug)]
 pub struct Parser {
-    pub tokens: Vec<(Token, Position)>,
+    pub tokens: Vec<Lexme>,
+    pos: usize,
 }
 
+#[allow(dead_code)]
 impl Parser {
-    pub fn run(&self) {}
+    pub fn new(tokens: Vec<Lexme>) -> Self {
+        Parser { tokens, pos: 0 }
+    }
+
+    fn peek(&self) -> Option<&Lexme> {
+        self.tokens.get(self.pos)
+    }
+    fn consume(&mut self) -> Option<Lexme> {
+        let tok = self.tokens.get(self.pos);
+        self.pos += 1;
+        tok.cloned()
+    }
+    // pub fn run(&mut self) -> Result<AST, Box<dyn std::error::Error>> {}
 }
